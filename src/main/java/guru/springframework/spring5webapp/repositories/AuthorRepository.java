d317e0534a6dee0e720b0c452643b27f650c2fc9
Exoget <ferchichi.feres@gmail.com>
2020-02-15 20:20:31 +0100

    update interface description

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 //Design pattern pour l'acces aux donnees ( persistance et query operations )
 //Interface qui contient les operations de bases ( ajout, modification , suppresion , recherche ...)
 //Lors du #runtime# spring fourni une implemntation pour nous, Spring DATA nous fourni l'implimentation de ces methodes.
+//  c est la calss #SimpleJpaRepository#
+//  qui fait partie du starter #spring-boot-starter-data-jpa# qui sera disponible dans le classpath container Spring
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
